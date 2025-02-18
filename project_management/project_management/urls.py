ac8b2c9cd9f8674e308dc0f8f7d53863644199a4
zatch2127 <145642291+zatch2127@users.noreply.github.com>
2025-02-18 13:25:57 +0530

    Update urls.py

--- project_management/project_management/urls.py
+++ project_management/project_management/urls.py
@@ -21,8 +21,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-
-    # ✅ Add JWT Token Endpoints
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
